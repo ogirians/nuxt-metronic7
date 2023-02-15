270e8d462fbd2012bc58d889d936a960c6bc19b7
ogirians <ogi.rians3@gmail.com>
2023-02-15 16:15:47 +0700

    first

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+   
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  head(){
+    return {
+      // js this page only
+      script : [
+        { 
+          hid: 'stripe4', 
+          src: '/assets/plugins/custom/fullcalendar/fullcalendar.bundle.js', 
+          // defer: true,
+          body:true,
+          // callback: () => { this.isStripeLoaded = true } 
+        },
+		    { 
+          hid: 'stripe5', 
+          src: '/assets/js/pages/widgets.js', 
+          // defer: true,
+          body:true,
+          // callback: () => { this.isStripeLoaded = true } 
+        },
+      ]
+        
+    }
+  },
+}
+</script>
